b4268ed9a29c8509f197190cd15e8abc4af6e458
Simon <simonschwarz.du@gmail.com>
2017-03-06 22:29:35 +0100

    network.txt updated

--- server1/event.py
+++ server1/event.py
@@ -1,18 +1,26 @@
 from json import JSONDecoder
+from server1 import *
+
+eventsExecute = {
+    "join": lambda world: pass,
+
+
+
+}
 
 
 def eventFromJSON(json):
     decode = JSONDecoder().decode(json)
     for key in decode:
-        if key == "join":
-            return JoinRequestEvent(decode[key])
-
-
+        return Event(key, **decode[key])
 
 class Event:
-    def __init__(self):
-        print("")
+    def __init__(self, name, **entries):
+        self.event = name
+        self.__dict__.update(entries)
 
-class JoinRequestEvent (Event):
-    def __init__(self, json):
-        self.name = json["name"]
+    def __init__(self, name):
+        self.eventName = name
+
+    def execute(self, world):
+
